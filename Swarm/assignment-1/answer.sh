91b5d5b644a4fafb9866a6f40f301015f3d0bc38
adhambek010 <adhamsalaydinov5@gmail.com>
2026-01-27 15:08:58 +0100

    registry

--- Swarm/assignment-1/answer.sh
+++ Swarm/assignment-1/answer.sh
@@ -1,17 +1,21 @@
-docker network create -d overlay back
-docker network create -d overlay front
-docker service create --name vote -p 80:80 --network front --replicas 2 \
+#! /bin/sh
+
+docker network create -d overlay backend
+
+docker network create -d overlay frontend
+
+docker service create --name vote -p 80:80 --network frontend --replicas 2 \
     bretfisher/examplevotingapp_vote
 
-docker service create --name redis --network front --replicas 2 \
-    redis    
-docker service create --name db --network back \
-    -e POSTGRES_HOST_AUTH_METHOD=trust --mount \
-    type=volume,source=db-data,target=/var/lib/postgresql/data \
-     postgres:alpine
+docker service create --name redis --network frontend redis:alpine
 
-docker service create --name worker --network front --network back \
+docker service create --name db --network backend \
+    -e POSTGRES_HOST_AUTH_METHOD=trust \
+    --mount type=volume,source=db-data,target=//var/lib/postgresql/data \
+    postgres:alpine
+
+docker service create --name worker --network frontend --network backend \
     bretfisher/examplevotingapp_worker
 
-docker service create --name result --network back -p 5001:80 \
+docker service create --name result --network backend -p 5001:80 \
     bretfisher/examplevotingapp_result
